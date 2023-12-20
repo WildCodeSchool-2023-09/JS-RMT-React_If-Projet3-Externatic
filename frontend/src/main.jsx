b28ee66d2cddcdb5c2ae8e5c9d745265f77fa8ea
hipticem <maamir.hipticem@gmail.com>
2023-12-20 16:38:27 +0100

    route front consultant

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import axios from "axios";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import ConsultantPage from "./pages/ConsultantPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/consultants",
+    element: <ConsultantPage />,
+    loader: async () => {
+      const consultant = await axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/api/consultants`)
+        .then((res) => res.data);
+      return consultant;
+    },
   },
 ]);
 
