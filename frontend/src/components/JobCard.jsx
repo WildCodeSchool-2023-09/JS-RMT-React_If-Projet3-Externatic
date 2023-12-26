1d0f053b75dea0c8a8a5ee6e4f726232555dc20f
OlvMrn <olivier.morin91@gmail.com>
2023-12-26 22:35:49 +0100

    création d'un contexte pour la gestion des favoris

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
+import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
+
 import "./JobCard.css";
+import { useJobContext } from "../contexts/context";
 
 function JobCard({ job }) {
+  const { favorites, manageFavorites } = useJobContext();
+
   const dateDiffInDaysFromToday = (date) => {
     const targetDate = new Date(date);
     const today = new Date();
@@ -13,24 +18,34 @@ function JobCard({ job }) {
 
   return (
     <div className="job-card">
-      <Link to={`/jobs/${job.id}`}>
-        <div className="job-card-body">
+      <div className="job-card-header">
+        <Link to={`/jobs/${job.id}`}>
           <h3 className="job-card-title">{job.title}</h3>
-          <div className="job-card-language">{job.language}</div>
-          <div className="job-card-position-category">
-            {job.position_category}
-          </div>
-          <div className="job-card-location">{job.location}</div>
-          <div className="job-card-salary">{job.salary}</div>
-          <div className="job-card-publish-date">
-            {dateDiffInDaysFromToday(job.created_at) === 0
-              ? "Offre publiée aujourd'hui"
-              : `Offre publiée il y a ${dateDiffInDaysFromToday(
-                  job.created_at
-                )} jour(s)`}
-          </div>
+        </Link>
+        <button
+          type="button"
+          aria-label="Add to favorites"
+          className="fav-button"
+          onClick={() => manageFavorites(job.id)}
+        >
+          {favorites.includes(job.id) ? <IoIosHeart /> : <IoIosHeartEmpty />}
+        </button>
+      </div>
+      <div className="job-card-body">
+        <div className="job-card-language">{job.language}</div>
+        <div className="job-card-position-category">
+          {job.position_category}
         </div>
-      </Link>
+        <div className="job-card-location">{job.location}</div>
+        <div className="job-card-salary">{job.salary}</div>
+        <div className="job-card-publish-date">
+          {dateDiffInDaysFromToday(job.created_at) === 0
+            ? "Offre publiée aujourd'hui"
+            : `Offre publiée il y a ${dateDiffInDaysFromToday(
+                job.created_at
+              )} jour(s)`}
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import connexion from "./services/connexion";
 import App from "./App";
 import AllJobsPage from "./pages/AllJobsPage";
 
+import { JobProvider } from "./contexts/context";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -29,6 +31,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <JobProvider>
+      <RouterProvider router={router} />
+    </JobProvider>
   </React.StrictMode>
 );
