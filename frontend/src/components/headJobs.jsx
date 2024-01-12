8deb24c73ebecc2b1cce2541af42a18cd8488629
iNaeRoZ <naeroz@outlook.fr>
2024-01-12 03:00:51 +0100

    restructuration de la page

--- frontend/src/components/headJobs.jsx
+++ frontend/src/components/headJobs.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 
+import "../styles/headJobs.css";
+
 function HeadJob() {
   const job = useLoaderData();
   return (
-    <div>
-      <div>{job.title}</div>
+    <div className="body">
+      <h1 className="title">{job.title}</h1>
+      <div className="resume_jobs">
+        <ul>
+          <br />
+          <li>Durée de travail:{job.working_type}</li>
+          <br />
+          <li>Niveau de formation:{job.position_requirements}</li>
+          <br />
+          <li>Lieu:{job.location}</li>
+        </ul>
+        <ul>
+          <br />
+          <li>Salaire:{job.salary}</li>
+          <br />
+          <li>Entrée en fonction:{job.starting_date}</li>
+          <br />
+          <li>CSP:{job.position_category}</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/jobId.jsx
+++ frontend/src/pages/jobId.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 
+import HeadJob from "../components/headJobs";
+
 import "../styles/jobId.css";
 
 function JobId() {
   const job = useLoaderData();
   return (
-    <div className="resume">
-      <div className="Title">{job.title}</div>
-      <br />
-      <div className="Description">
-        <div>{job.description_mission}</div>
+    <div>
+      <HeadJob />
+      <div className="resume">
         <br />
-        <div>{job.description_about_candidate}</div>
-        <div>{job.description_position}</div>
-        <div>{job.description_process}</div>
+        <div className="Description">
+          <h1>Les missions</h1>
+          <div>{job.description_mission}</div>
+          <br />
+          <h2>Ce que vous apportez</h2>
+          <div>{job.description_about_candidate}</div>
+          <br />
+          <h2>Les + du poste</h2>
+          <div>{job.description_position}</div>
+          <h2>Le processus</h2>
+          <div>{job.description_process}</div>
+        </div>
+        <div>{job.language}</div>
+        <div>{job.salary}</div>
+        <div>{job.location}</div>
+        <div>{job.working_hours}</div>
+        <div>{job.starting_date}</div>
+        <div>{job.created_at}</div>
       </div>
-      <div>{job.language}</div>
-      <div>{job.salary}</div>
-      <div>{job.location}</div>
-      <div>{job.working_hours}</div>
-      <div>{job.starting_date}</div>
-      <div>{job.created_at}</div>
     </div>
   );
 }
