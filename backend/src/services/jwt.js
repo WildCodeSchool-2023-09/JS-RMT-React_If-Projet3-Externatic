034bfd2c46dc18536b67d3c27e2a0c0aef9e9ece
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2024-01-24 15:25:21 +0100

    Update jwt.js

--- backend/src/services/jwt.js
+++ backend/src/services/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const createToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET);
+  return jwt.sign(payload, process.env.APP_SECRET);
 };
 
 const verifyToken = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET);
+  return jwt.verify(token, process.env.APP_SECRET);
 };
 
 module.exports = { createToken, verifyToken };
