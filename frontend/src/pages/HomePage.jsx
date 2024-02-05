56b003245c77679333ddf53c6e328a95d24296c8
OlvMrn <olivier.morin91@gmail.com>
2024-02-05 23:56:09 +0100

    ajout symboles + et - dans collapser, et ajour barre de recherche sur page jobs

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -23,7 +23,7 @@ function HomePage() {
           <h1 className="externatic-red">Externatic,</h1>
           <h2>plus qu'un cabinet de recrutement informatique</h2>
         </p>
-        <SearchBar />
+        <SearchBar page="home" />
       </div>
       <div className="red-line" />
       <Carousel jobs={allJobsData} />
